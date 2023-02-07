5feda896d133f94167b1ee9e0a4da1b18cc64929
Ivan Mayor <maramorosh2@gmail.com>
2023-02-07 20:53:44 +0200

    Add model TaskType related with Task model by ForeignKey

--- task_manager/models.py
+++ task_manager/models.py
@@ -2,25 +2,36 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+
 class Task(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     deadline = models.DateField()
     is_completed = models.BooleanField(default=False)
+
     HIGH = "HI"
     MEDIUM = "MED"
     LOW = "LOW"
+
     PRIORITY_CHOICES = [
         (HIGH, "High"),
         (MEDIUM, "Medium"),
         (LOW, "Low")
     ]
+
     priority = models.CharField(
         max_length=3,
         choices=PRIORITY_CHOICES,
         default=MEDIUM
     )
-    # task_type = models.OneToOneField()
+    task_type = models.OneToOneField(
+        TaskType,
+        on_delete=models.CASCADE
+    )
+    # assignees = ...
 
 
 class Worker(AbstractUser):
