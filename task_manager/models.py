24dcd6729a191881dc7d59d4bfd84290c7f787b3
Ivan Mayor <maramorosh2@gmail.com>
2023-02-07 20:41:01 +0200

    Create model Task

--- task_manager/models.py
+++ task_manager/models.py
@@ -2,5 +2,26 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    HIGH = "HI"
+    MEDIUM = "MED"
+    LOW = "LOW"
+    PRIORITY_CHOICES = [
+        (HIGH, "High"),
+        (MEDIUM, "Medium"),
+        (LOW, "Low")
+    ]
+    priority = models.CharField(
+        max_length=3,
+        choices=PRIORITY_CHOICES,
+        default=MEDIUM
+    )
+    # task_type = models.OneToOneField()
+
+
 class Worker(AbstractUser):
     pass
