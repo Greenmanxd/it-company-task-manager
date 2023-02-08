8466eae0097d5f30e7b5b442a35c2bc9c211fe9d
Ivan Mayor <maramorosh2@gmail.com>
2023-02-08 14:43:19 +0200

    Make migrations and add string representations to models

--- task_manager/models.py
+++ task_manager/models.py
@@ -5,9 +5,15 @@ from django.db import models
 class TaskType(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Position(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class Worker(AbstractUser):
@@ -17,27 +23,26 @@ class Worker(AbstractUser):
         default=1
     )
 
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name}) {self.position}"
+
 
 class Task(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    deadline = models.DateField()
+    deadline = models.DateField(max_length=12)
     is_completed = models.BooleanField(default=False)
 
-    HIGH = "HI"
-    MEDIUM = "MED"
-    LOW = "LOW"
-
     PRIORITY_CHOICES = (
-        (HIGH, "High"),
-        (MEDIUM, "Medium"),
-        (LOW, "Low")
+        ("HI", "High"),
+        ("MED", "Medium"),
+        ("LOW", "Low"),
     )
 
     priority = models.CharField(
         max_length=3,
         choices=PRIORITY_CHOICES,
-        default=MEDIUM
+        default="MED"
     )
     task_type = models.ForeignKey(
         TaskType,
@@ -45,3 +50,6 @@ class Task(models.Model):
     )
 
     assignees = models.ManyToManyField(Worker, related_name="tasks")
+
+    def __str__(self):
+        return f"{self.name}: {self.description}"
