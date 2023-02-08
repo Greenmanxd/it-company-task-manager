2d1a29c345ab5b9a59a77a44cd7e2152c50cdd7f
Ivan Mayor <maramorosh2@gmail.com>
2023-02-08 15:24:02 +0200

    Set up fields (list_display, fieldsets, add_fieldsets) for Worker in admin panel

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -6,7 +6,24 @@ from task_manager.models import Worker, Task, TaskType, Position
 
 @admin.register(Worker)
 class WorkerAdmin(UserAdmin):
-    pass
+    list_display = UserAdmin.list_display + ("position", )
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position", )}), )
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
 
 
 @admin.register(Task)
