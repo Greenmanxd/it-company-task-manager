96847c85ea45ad9dcefc9b6f984f381ba3b14713
Ivan Mayor <maramorosh2@gmail.com>
2023-02-08 11:56:58 +0200

    Register models in admin panel

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from task_manager.models import Worker, Task, TaskType, Position
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    pass
