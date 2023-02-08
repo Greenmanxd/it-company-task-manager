7fd2c19768abf59d04383f7740a3a1c05b153fb7
Ivan Mayor <maramorosh2@gmail.com>
2023-02-08 16:26:23 +0200

    Remake index view with render(request, ...) to html file

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Task_manager")
+    return render(request, "task_manager/index.html")
