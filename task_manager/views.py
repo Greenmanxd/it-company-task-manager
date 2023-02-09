3c3efc23bf30e10e5d93d0eba2f604247c26bd8e
Ivan Mayor <maramorosh2@gmail.com>
2023-02-09 14:10:05 +0200

    Add simple home page with info about workers, tasks, number of visits

--- task_manager/views.py
+++ task_manager/views.py
@@ -6,18 +6,21 @@ from task_manager.models import Task, TaskType, Worker, Position
 def index(request):
 
     num_tasks = Task.objects.count()
+    in_proces_tasks = Task.objects.filter(is_completed=False).count()
     num_task_types = TaskType.objects.count()
     num_workers = Worker.objects.count()
     num_positions = Position.objects.count()
 
-    num_visits = request.session.get("num_visits", 0)
+    num_visits = request.session.get("num_visits", 1)
     request.session["num_visits"] = num_visits + 1
 
     context = {
         "num_task_types": num_task_types,
         "num_positions": num_positions,
         "num_workers": num_workers,
-        "num_tasks": num_tasks
+        "num_tasks": num_tasks,
+        "in_proces_tasks": in_proces_tasks,
+        "num_visits": num_visits
     }
 
     return render(request, "task_manager/index.html", context=context)
