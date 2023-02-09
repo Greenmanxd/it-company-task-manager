b90f0147c1c30ae35869f321d7e1e882af270a13
Ivan Mayor <maramorosh2@gmail.com>
2023-02-09 13:46:20 +0200

    Add context to index view with numbers of workers, tasks, task types, positions and session visits

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+from task_manager.models import Task, TaskType, Worker, Position
+
 
 def index(request):
-    return render(request, "task_manager/index.html")
+
+    num_tasks = Task.objects.count()
+    num_task_types = TaskType.objects.count()
+    num_workers = Worker.objects.count()
+    num_positions = Position.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_task_types": num_task_types,
+        "num_positions": num_positions,
+        "num_workers": num_workers,
+        "num_tasks": num_tasks
+    }
+
+    return render(request, "task_manager/index.html", context=context)
