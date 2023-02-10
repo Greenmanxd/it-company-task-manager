da7a2ef4cd23c88366aeeb37daf159b7d73eba2a
Ivan Mayor <maramorosh2@gmail.com>
2023-02-10 14:10:37 +0200

    Add detail page for users (workers)

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,11 +1,23 @@
 from django.urls import path
 
-from task_manager.views import index, WorkerListView, TaskListView, TaskTypeView
+from task_manager.views import (
+    index,
+    WorkerListView,
+    TaskListView,
+    TaskTypeView,
+    WorkerCreateView,
+    WorkerDetailView
+)
 
 urlpatterns = [
     path("", index, name="index"),
+
     path("workers/", WorkerListView.as_view(), name="worker-list"),
+    path("worker/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
+    path("workers/create/", WorkerCreateView.as_view(), name="worker-create"),
+
     path("tasks/", TaskListView.as_view(), name="task-list"),
+
     path("task-types/", TaskTypeView.as_view(), name="task-type-list")
 ]
 
